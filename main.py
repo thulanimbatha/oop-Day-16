69bb4aac6aecd853d5da86eafa698c64439ff0ba
thulanimbatha <thulanimbatha06@gmail.com>
2021-09-29 01:04:28 +0200

    added loop for questions

--- main.py
+++ main.py
@@ -10,4 +10,9 @@ for question in question_data:
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print("END")
+
--- quiz_brain.py
+++ quiz_brain.py
@@ -11,3 +11,9 @@ class QuizBrain:
         current_question = self.questions_list[self.question_number]
         self.question_number += 1
         input(f"Q.{self.question_number}: {current_question.text} (True/False): ")
+
+    def still_has_questions(self):
+        '''Returns True if there are still questions left to ask'''
+        if self.question_number < len(self.questions_list):
+            return True
+        return False
